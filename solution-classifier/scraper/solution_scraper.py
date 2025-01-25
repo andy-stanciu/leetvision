43efc4e9a61c08997f4f63f33e43e567b150982f
Andy Stanciu <andreist@outlook.com>
2025-01-24 17:40:54 -0800

    Working parser

--- solution-classifier/scraper/solution_scraper.py
+++ solution-classifier/scraper/solution_scraper.py
@@ -7,14 +7,20 @@ from scraper_constants import *
 class SolutionScraper():
     def __init__(self, questions):
         with open(questions, 'r') as file:
-            self.questions = file.read().strip().split(',')
-            self.questions = [q.strip() for q in self.questions]
+            self.questions = dict(
+                (k.strip(), v.strip())
+                for k, v in (item.split(':') for item in file.read().strip().split(', '))
+            )
 
-    def scrape_solutions(self, language, solution_count=10, question_count=-1):
+    def scrape_solutions(self, solution_count=10, question_count=-1):
         if question_count == -1 or question_count > len(self.questions):
             question_count = len(self.questions)
-        for question in self.questions[:question_count]:
-            question_folder = SOLUTION_DIR + f'{str(language)}/{question}'
+        
+        question_idx = 0
+        for question, header in self.questions.items():
+            if question_idx == question_count:
+                break
+            question_folder = SOLUTION_DIR + question
             if (os.path.exists(question_folder)):
                 print(f'Skipped {question}, reason: already scraped')
                 continue
@@ -26,7 +32,7 @@ class SolutionScraper():
             solutions = set()
             while i < solution_count:
                 original_i = i
-                solution_ids = scrape_solution_ids(question, language, 
+                solution_ids = scrape_solution_ids(question, 
                                                    skip=page_idx * solution_count * 2, 
                                                    count=solution_count * 2)
                 for solution_id in solution_ids:
@@ -35,7 +41,7 @@ class SolutionScraper():
                     if solution_id in solutions:
                         continue
                     solutions.add(solution_id)
-                    for solution in scrape_solution_code(solution_id, language):
+                    for solution in scrape_solution_code(solution_id, header):
                         if i == solution_count:
                             break
                         path = os.path.join(question_folder, f'{question}-{i + 1}.txt')
@@ -47,9 +53,10 @@ class SolutionScraper():
                 page_idx += 1
             
             print(f'Scraped {i} LeetCode solutions to {question_folder}')
+            question_idx += 1
 
 
-def scrape_solution_code(solution_id, language):
+def scrape_solution_code(solution_id, header):
     response = requests.post(LEETCODE_GRAPHQL_URL, 
         json = {
             "query": communitySolution_query, 
@@ -76,27 +83,31 @@ def scrape_solution_code(solution_id, language):
         code_blocks = re.findall(r'```(.*?)```', solution_raw, re.DOTALL)
 
         for code_block in code_blocks:
+            if header.lower() not in code_block.lower(): # not a solution
+                continue
+            code_block = re.sub(r'[\x00-\x1F]+', '', code_block) # fix control sequences
             code_block = code_block.replace("\\n", "\n")  # fix newlines
             code_block = code_block.replace("\\t", "\t")  # fix tabs
-            code_block = code_block.replace("\'", "'")  # fix apostrophes
+            code_block = code_block.replace("\\'", "'")  # fix apostrophes
+            code_block = code_block.replace("\\\\'", "\\")  # fix backslashes
             
             language_header = code_block.splitlines()[0].strip().lower()
-            if (language_header in LANGUAGE_MAPPINGS and LANGUAGE_MAPPINGS[language_header] == language):  # language matches
+            if (language_header in LANGUAGE_MAPPINGS):  # language header found
                 code_block = re.sub(r'^.*\n', '', code_block, count=1)  # remove first line, i.e. language header
-                solutions.append(code_block)
+            solutions.append(code_block)
         
         return solutions
     else:
         print(f"Request failed with status code {response.status_code}: {response.text}")
 
 
-def scrape_solution_ids(question_name, language, skip, count):
+def scrape_solution_ids(question_name, skip, count):
     response = requests.post(LEETCODE_GRAPHQL_URL, 
         json = {
             "query": communitySolutions_query, 
             "variables": {
                 "query": "",
-                "languageTags": str(language),
+                "languageTags": "",
                 "topicTags": "",
                 "questionSlug": question_name,
                 "skip": skip,
@@ -121,14 +132,7 @@ def scrape_solution_ids(question_name, language, skip, count):
 
 def main():
     scraper = SolutionScraper(REGULAR_QUESTIONS)
-    scraper.scrape_solutions(Language.JAVA, question_count=3)
-    scraper.scrape_solutions(Language.CPP, question_count=3)
-    scraper.scrape_solutions(Language.C, question_count=3)
-    scraper.scrape_solutions(Language.CSHARP, question_count=3)
-    scraper.scrape_solutions(Language.PYTHON, question_count=3)
-    scraper.scrape_solutions(Language.JS, question_count=3)
-    scraper.scrape_solutions(Language.TS, question_count=3)
-    scraper.scrape_solutions(Language.GO, question_count=3)
+    scraper.scrape_solutions(solution_count=100, question_count=5)
 
 if __name__ == "__main__":
     main()
