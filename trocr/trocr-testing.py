5aa3ad817ddc65949d51723415512be5b373ee19
Rich Chen <richchen2002@gmail.com>
2025-01-30 10:57:45 -0800

    TrOCR preliminary results

--- trocr/trocr-testing.py
+++ trocr/trocr-testing.py
@@ -3,6 +3,9 @@ from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 import requests
 
 from PIL import Image
+import time
+
+start_time = time.time()
 
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
 
@@ -10,12 +13,19 @@ model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwrit
 
 # load image from the IAM dataset
 
-url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02.jpg"
-
-image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+# url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02.jpg"
+# image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+image_path = "/home/rich/Downloads/Image2.png"
+image = Image.open(image_path).convert("RGB")
 
 pixel_values = processor(image, return_tensors="pt").pixel_values
 
 generated_ids = model.generate(pixel_values)
 
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print(generated_text)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.4f} seconds")
