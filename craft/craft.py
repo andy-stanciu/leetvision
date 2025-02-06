85f2b8b7e5dd8bda4ad42f7bb5e69be5b29984da
Rich Chen <richchen2002@gmail.com>
2025-02-06 13:33:48 -0800

    CRAFT testing

--- craft/craft.py
+++ craft/craft.py
@@ -1,14 +1,20 @@
 import warnings
 import numpy as np
-
+import os
 from craft_text_detector import Craft
-warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
 
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
-image = '../sample-images/1.jpg'
+image_dir = '../sample-images/'
 output_dir = 'outputs/'
+
 craft = Craft(output_dir=output_dir, crop_type="poly", cuda=False)
-prediction_result = craft.detect_text(image)
+
+for filename in os.listdir(image_dir):
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  
+        image_path = os.path.join(image_dir, filename)
+        print(f"Processing: {image_path}")
+        prediction_result = craft.detect_text(image_path)
 
 craft.unload_craftnet_model()
 craft.unload_refinenet_model()
