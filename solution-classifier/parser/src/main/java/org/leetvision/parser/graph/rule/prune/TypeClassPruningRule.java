e3f1f79f29267e2d5ead2e54cc162df746b41f5b
Andy Stanciu <andreist@outlook.com>
2025-02-20 12:08:23 -0800

    Finish C# language mappings

--- solution-classifier/parser/src/main/java/org/leetvision/parser/graph/rule/prune/TypeClassPruningRule.java
+++ solution-classifier/parser/src/main/java/org/leetvision/parser/graph/rule/prune/TypeClassPruningRule.java
@@ -1,11 +1,11 @@
-package org.leetvision.parser.reduction.rule;
+package org.leetvision.parser.graph.rule.prune;
 
 import static org.leetvision.parser.meta.MetaLanguage.MetaNode;
 
-public final class TypeArgumentsPruningRule implements IPruningRule {
+public final class TypeClassPruningRule implements IPruningRule {
     @Override
     public boolean shouldPrune(MetaNode parent, MetaNode child) {
-        if (parent != MetaNode.TYPE_ARGUMENTS) {
+        if (parent != MetaNode.TYPE_CLASS) {
             return false;
         }
         return child == MetaNode.OP_LT || child == MetaNode.OP_GT;
