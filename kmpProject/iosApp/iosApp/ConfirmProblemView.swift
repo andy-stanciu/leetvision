9fdb21dea92bf4cbd03225ece91944ba7a5432ba
ashay <ashay9@cs.washington.edu>
2025-02-25 08:37:42 -0800

    Template pipeline

--- kmpProject/iosApp/iosApp/ConfirmProblemView.swift
+++ kmpProject/iosApp/iosApp/ConfirmProblemView.swift
@@ -3,19 +3,32 @@ import SwiftUI
 struct ConfirmProblemView: View {
     var body: some View {
         VStack {
-            Text("Confirm Problem")
-                .font(.largeTitle)
-                .padding()
+//            Text("Confirm Problem")
+//                .font(.largeTitle)
+//                .padding()
+
+            Spacer()
+
+            NavigationLink(destination: MainAppView(problem: "Same Tree")) {
+                Text("Same Tree")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+
+            NavigationLink(destination: MainAppView(problem: "Symmetric Tree")) {
+                Text("Symmetric Tree")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+
             Spacer()
         }
         .navigationBarTitle("Confirm Problem", displayMode: .inline)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: Button(action: {
-            // Action to go back
-            // In SwiftUI, the back button is handled automatically by NavigationView
-        }) {
-            Image(systemName: "arrow.left")
-                .foregroundColor(.blue)
-        })
     }
 }
