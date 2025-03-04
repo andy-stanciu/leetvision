9f05ac921989472751f1a90180abf3ce8ab2f65d
Rich Chen <richchen2002@gmail.com>
2025-03-03 18:57:59 -0800

    End-to-end working

--- solution-recognizer/main.py
+++ solution-recognizer/main.py
@@ -9,7 +9,7 @@ image_path = "handwritten2.jpg"
 image_name, image_extension = os.path.splitext(image_path)
 
 # Phase 1: Run Phase1_CRAFTSegmentation.py
-phase1(image_path)
+phase1("sample-images/" + image_path)
 
 # Phase 2: Run Phase2_TesseractRecognition.py
 raw_ocr_text = phase2(image_name)
